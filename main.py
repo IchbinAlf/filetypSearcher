071480265870e8155a6eb7430b8907066e7dc204
IchbinAlf <52667007+IchbinAlf@users.noreply.github.com>
2022-01-29 00:37:23 +0100

    Funktion - mit Input-Var

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ import os
 #   os.walk in eine Funktion verpacken (mit variablem "path" und "endswith")
 #   -> Datei speichern als Grundlage für weitere Projekte
 
-#   AI-SVG-Konverter mit filetypSearcher als Grundlage
+while True:
+    path = "../../Documents/00_Dokumente"
+    filesearch = input("Which file are you searching?")
+    for root, dirs, files in os.walk(path):
+        for x in files:
+            if x.endswith(filesearch):
+                print(x)
+    break        
 
-# path = "../../Documents/00_Dokumente"
-
-# for root, dirs, files in os.walk(path):
-#     for x in files:
-#         if x.endswith('.txt'):
-#             print(x)
+#Input prüfen: Was passiert falls kein String || kein "." als führends Zeichen || etc. eingegeben wird?
 
 # Python PATH - weiter verfolgen
-import pywin32.client as win32
+# import win32com.client as win32
