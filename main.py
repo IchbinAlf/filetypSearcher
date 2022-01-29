db994dbe33d5744b386e61f553d1ffb2866b12da
IchbinAlf <52667007+IchbinAlf@users.noreply.github.com>
2022-01-29 12:31:33 +0100

    Lösung für Problem mit Interpreter

--- main.py
+++ main.py
@@ -14,5 +14,8 @@ while True:
 
 #Input prüfen: Was passiert falls kein String || kein "." als führends Zeichen || etc. eingegeben wird?
 
-# Python PATH - weiter verfolgen
-# import win32com.client as win32
+# Lösung für PyLance-Problem (Module konnten nicht gefunden werden)
+# -> in VSCode wurde der der falsche Interpreter ausgewählt ((Ctrl+Shift+P) -> Python Select Interpreter)
+# reddit: https://www.reddit.com/r/vscode/comments/o67qm5/import_could_not_be_resolved_pylance/
+
+import win32com.client as win32
